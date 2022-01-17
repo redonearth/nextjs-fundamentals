eac140d5eb49c43503c1a1b35c97970585c4a4a1
Jisang Hong <wona23@gmail.com>
2022-01-17 09:48:12 +0900

    Styled JSX 추가

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,13 +6,22 @@ export default function Navbar() {
   return (
     <nav>
       <Link href="/">
-        <a style={{ color: router.pathname === '/' ? 'red' : 'blue' }}>Home</a>
+        <a className={router.pathname === '/' ? 'active' : ''}>Home</a>
       </Link>
       <Link href="/about">
-        <a style={{ color: router.pathname === '/about' ? 'red' : 'blue' }}>
-          About
-        </a>
+        <a className={router.pathname === '/about' ? 'active' : ''}>About</a>
       </Link>
+      <style jsx>{`
+        nav {
+          background-color: tomato;
+        }
+        a {
+          text-decoration: none;
+        }
+        .active {
+          color: yellow;
+        }
+      `}</style>
     </nav>
   );
 }
