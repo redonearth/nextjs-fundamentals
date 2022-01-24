fee7af7e2a109aa0a7e460336411299fa93d1d24
Jisang Hong <wona23@gmail.com>
2022-01-24 16:25:20 +0900

    Redirect, Rewrite 적용(API 키 숨김 처리)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const API_KEY = process.env.API_KEY;
 
 /** @type {import('next').NextConfig} */
 module.exports = {
@@ -8,5 +9,22 @@ module.exports = {
   },
   images: {
     domains: ['image.tmdb.org'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/contact',
+        destination: '/form',
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/movies',
+        destination: `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+      },
+    ];
   },
 };
