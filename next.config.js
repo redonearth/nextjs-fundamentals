d2a96ef4d3c237cf90e38a4f37b1c52df6352de2
Jisang Hong <wona23@gmail.com>
2022-01-24 15:47:54 +0900

    포스터 이미지 설정, 스타일링 추가

--- next.config.js
+++ next.config.js
@@ -6,4 +6,7 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  images: {
+    domains: ['image.tmdb.org'],
+  },
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import SEO from '../components/SEO';
 
@@ -6,6 +7,7 @@ const API_KEY = '5efe6813c047c5a28d580b92e30a6262';
 interface IMovieData {
   id: string;
   original_title: string;
+  poster_path: string;
 }
 
 export default function Home() {
@@ -21,14 +23,50 @@ export default function Home() {
     })();
   }, []);
   return (
-    <div>
+    <div className="container">
       <SEO title="Home" />
       {!movies && <h4>Loading...</h4>}
       {movies?.map((movie: IMovieData) => (
-        <div key={movie.id}>
+        <div className="movie" key={movie.id}>
+          <div className="poster">
+            <Image
+              layout="fill"
+              objectFit="cover"
+              src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+              alt={`${movie.original_title} Poster`}
+            />
+          </div>
           <h4>{movie.original_title}</h4>
         </div>
       ))}
+      <style jsx>{`
+        .container {
+          display: grid;
+          grid-template-columns: 1fr 1fr;
+          padding: 20px;
+          gap: 20px;
+        }
+        .movie {
+          &:hover {
+            .poster {
+              transform: scale(1.05) translateY(-10px);
+            }
+          }
+          .poster {
+            position: relative;
+            width: 230px;
+            height: 345px;
+            border-radius: 12px;
+            overflow: hidden;
+            box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+            transition: transform 0.2s ease-in-out;
+          }
+          h4 {
+            font-size: 18px;
+            text-align: center;
+          }
+        }
+      `}</style>
     </div>
   );
 }
